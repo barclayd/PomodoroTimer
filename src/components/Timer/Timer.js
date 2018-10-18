96a5b2ed1918912dd44242138e5ecd9a2194b538
Daniel Barclay <barclaysd@me.com>
2018-10-18 23:20:38 +0100

    countdown method added to allow timer to show time elapsed

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -16,9 +16,11 @@ class Timer extends Component {
             currentTime: moment.duration(25, 'minutes'),
             baseTime: moment.duration(25,'minutes'),
             timerState: TimerStates.NOT_SET,
+            timer: null,
         };
         this.baseTimeHandler = this.baseTimeHandler.bind(this);
         this.startTimer = this.startTimer.bind(this);
+        this.countdown = this.countdown.bind(this);
     }
 
     baseTimeHandler(newBaseTime) {
@@ -30,10 +32,19 @@ class Timer extends Component {
 
     startTimer() {
         this.setState({
-            timerState: TimerStates.RUNNING
-        })
+            timerState: TimerStates.RUNNING,
+            timer: setInterval(this.countdown, 1000)
+        });
+
     }
 
+    countdown() {
+        const newTime = moment.duration(this.state.currentTime);
+        newTime.subtract(1, 'second');
+        this.setState({
+            currentTime: newTime
+        });
+    }
 
 
     render() {
