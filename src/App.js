df9f131f858042f869cd06d727b9f7d08ff730fc
Daniel Barclay <barclaysd@me.com>
2018-10-18 19:57:21 +0100

    Separated concerns into relevant components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Timer from './components/Timer/Timer';
+import TimerHeader from './components/TimerHeader/TimerHeader';
+import TimerDisplay from "./components/TimerDisplay/TimerDisplay";
+import TimerButton from "./components/TimerButton/TimerButtomn";
+import TimerConfig from "./components/TimerConfig/TimerConfig";
 
 class App extends Component {
   render() {
     return (
       <div className="panel panel-default app-content center-block">
           <div className="panel-body">
-              <Timer />
+              <TimerHeader/>
+              <TimerDisplay/>
+              <TimerButton/>
+              <TimerConfig/>
           </div>
       </div>
     );
