c8a4df59543edd44ce3a7e4fb7cb44ccd5b30834
Daniel Barclay <barclaysd@me.com>
2018-10-18 19:32:06 +0100

    Added static content to App, created Timer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Timer from './components/Timer/Timer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World!
+      <div className="panel panel-default app-content center-block">
+          <div className="panel-body">
+              <Timer />
+          </div>
       </div>
     );
   }
